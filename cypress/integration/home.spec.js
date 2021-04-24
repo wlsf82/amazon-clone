2c6569a358036074408c6312db7bb5ea97b10f02
Walmyr <wlsf82@gmail.com>
2021-04-24 15:53:57 +0200

    Implement home page test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,18 @@
 describe('Home', () => {
   beforeEach(() => cy.visit('/'))
 
-  it('lists five products and add 2 to the shopping cart', () => {})
+  it('lists five products and add 2 to the shopping cart', () => {
+    cy.get('.product')
+      .should('have.length', 5)
+
+    Cypress._.times(2, () => {
+      cy.get('.product button')
+        .first()
+        .click()
+    })
+
+    cy.get('.header__basketCount')
+      .should('be.visible')
+      .and('have.text', '2')
+  })
 })
