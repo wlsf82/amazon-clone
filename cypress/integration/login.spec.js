e8eae4768662c71d4150bac1b01aa84cc8ee52df
Walmyr <wlsf82@gmail.com>
2021-04-24 16:30:41 +0200

    Implement login page tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,15 +1,68 @@
 describe('Login', () => {
   beforeEach(() => cy.visit('/login'))
 
-  it('alterts about badly formatted email', () => {})
+  it('alterts about badly formatted email', () => {
+    cy.get('[type="email"]')
+      .type('invalid-email.com')
+    cy.get('.login__signInBtn')
+      .click()
 
-  it('alterts about invalid password', () => {})
+    cy.on("window:alert", str => expect(str).to.equal(
+      'The email address is badly formatted.'
+    ))
+  })
 
-  it('alerts about unnexisting user', () => {})
+  it('alterts about invalid password', () => {
+    cy.get('[type="email"]')
+      .type('walmyr@example.com')
+    cy.get('[type="password"]')
+      .type('invalid')
+    cy.get('.login__signInBtn')
+      .click()
 
-  it('successfully logs in', () => {})
+    cy.on("window:alert", str => expect(str).to.equal(
+      'The password is invalid or the user does not have a password.'
+    ))
+  })
+
+  it('alerts about unnexisting user', () => {
+    cy.get('[type="email"]')
+      .type('unnexisting-user@example.com')
+    cy.get('[type="password"]')
+      .type('somepassword')
+    cy.get('.login__signInBtn')
+      .click()
+
+    cy.on("window:alert", str => expect(str).to.contain(
+      'There is no user record corresponding to this identifier. The user may have been deleted.'
+    ))
+  })
+
+  it('successfully logs in', () => {
+    cy.get('[type="email"]')
+      .type(Cypress.env('user_email'))
+    cy.get('[type="password"]')
+      .type(Cypress.env('user_password'), { log: false })
+    cy.get('.login__signInBtn')
+      .click()
+
+    cy.get('.header__optionLineOne')
+      .contains(`Hello ${Cypress.env('user_email')}`)
+      .should('be.visible')
+  })
 
   context('Create acount', () => {
-    it('alerts about already existing user', () => {})
+    it('alerts about already existing user', () => {
+      cy.get('[type="email"]')
+        .type(Cypress.env('user_email'))
+      cy.get('[type="password"]')
+        .type('some-other-secret-password')
+      cy.get('.login__registerBtn')
+        .click()
+
+      cy.on("window:alert", str => expect(str).to.equal(
+        'The email address is already in use by another account.'
+      ))
+    })
   })
 })
